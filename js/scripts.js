1939c5e11a0781f42bd07e08e12fe6d0a7703269
Lucian <lucian.miller185@gmail.com>
2021-03-09 09:45:08 -0800

    It will cause first and last character to be capitalized.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
   let userSentence = prompt("Write a sentence:");
+  console.log(userSentence)
   upperCaseFirstLast(userSentence);
 });
 
 const upperCaseFirstLast = function(sentence) {
-  let firstChar = sentence.charAt(0);
-  let lastChar = sentence.charAt(sentence.length-1);
+  let firstChar = sentence.charAt(0).toUpperCase();
+  let lastChar = sentence.charAt(sentence.length-1).toUpperCase();
 
   console.log("length: " + sentence.length);
   console.log("first char: " + firstChar);
   console.log("last char: " + lastChar);
-  alert("first char: " + firstChar + "last char: " + lastChar);
+  alert("first char: " + firstChar + " last char: " + lastChar);
   return;
 };
