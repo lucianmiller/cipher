e9b55497baa21ff8ec5b1148d8b0a64868524e45
Lucian <lucian.miller185@gmail.com>
2021-03-10 10:31:17 -0800

    It will delete console logs.

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,21 @@
 $(document).ready(function() {
   let userSentence = prompt("Write a sentence:");
-  console.log(userSentence)
   alert(mainFunction(userSentence));
 });
 
 const upperCaseFirstLast = function(sentence) {
   let firstChar = sentence.charAt(0).toUpperCase();
   let lastChar = sentence.charAt(sentence.length-1).toUpperCase();
-
-  console.log("length: " + sentence.length);
-  console.log("first char: " + firstChar);
-  console.log("last char: " + lastChar);
-  alert("first char: " + firstChar + " last char: " + lastChar);
   return firstChar + lastChar;
 };
 
 const reverseFirstLast = function(chars) {
   reverseString = chars.charAt(1) + chars.charAt(0);
-  console.log(reverseString);
   return reverseString;
 };
 
 const halfOfSentence = function(sentence) {
   let oneHalf = sentence.charAt(sentence.length / 2);
-  console.log(oneHalf);
   return oneHalf;
 };
 
@@ -31,7 +23,5 @@ const mainFunction = function(string) {
   const capLetters = upperCaseFirstLast(string);
   let newString = halfOfSentence(string) + string + reverseFirstLast(capLetters);
   let finalCipher = newString.split("").reverse().join("");
-  console.log(newString);
-  console.log(finalCipher);
   return finalCipher;
 };
